6ce96ee133e53eb6016ed64441a11dd81491dbd1
AlqamaShuja <alqamashuja313@gmail.com>
2022-07-18 09:54:32 +0500

    new Class Student Created

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ class Person {
     }
 }
 
-let p1 = new Person("Ali", 30, 3456);
-console.log(p1.name);
+// let p1 = new Person("Ali", 30, 3456);
+// p1.showDetails()
 
-let p2 = new Person();
-p2.name = "Usama";
-console.log(p2);
+class Student extends Person {
+    constructor(n, age, id, sem, field){
+        super(n, age, id);
+        this.semester = sem;
+        this.fieldOfStudy = field;
+    }
+
+    showDetails(){
+        console.log(`My Name is ${this.name}`);
+    }
+}
+
+
+let std1 = new Student("Waqar", 12, 54321, "4th Semester", "IT");
+console.log(std1.name);
